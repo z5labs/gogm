1b23c7953c58f7d6e499d077cc7991bb1a6413a5
Eric Solender <ericsolender@gmail.com>
2019-07-12 16:19:48 -0400

    create indexes are there

--- index.go
+++ index.go
@@ -19,12 +19,14 @@ func dropAllIndexes() error{
 	}
 
 	dropSess := dsl.NewSession()
+	defer dropSess.Close()
 	err = dropSess.Begin()
 	if err != nil{
 		return err
 	}
 
 	for _, constraint := range rowsStr{
+		log.Debugf("dropping constraint '%s'", constraint)
 		_, err := dropSess.Query().Cypher(fmt.Sprintf("DROP %s", constraint)).Exec(nil)
 		if err != nil{
 			oerr := err
@@ -42,12 +44,99 @@ func dropAllIndexes() error{
 
 //creates all indexes
 func createAllIndexes() error{
+	//validate that we have to do anything
 	if mappedTypes == nil || len(mappedTypes) == 0{
 		return errors.New("must have types to map")
 	}
+
+	numIndexCreated := 0
+
+	//setup session
+	sess := dsl.NewSession()
+	defer sess.Close()
+	err := sess.Begin()
+	if err != nil{
+		return err
+	}
+
+	//index and/or create unique constraints wherever necessary
+	for node, structConfig := range mappedTypes{
+		if structConfig.Fields == nil || len(structConfig.Fields) == 0{
+			continue
+		}
+
+		var indexFields []string
+
+		for fieldName, config := range structConfig.Fields{
+			//pk is a special unique key
+			if config.PrimaryKey || config.Unique{
+				for _, label := range structConfig.Labels{
+					numIndexCreated++
+
+					_, err := sess.Query().Create(dsl.NewConstraint(&dsl.ConstraintConfig{
+						Unique: true,
+						Name: node,
+						Type: label,
+						Field: fieldName,
+					})).Exec(nil)
+					if err != nil{
+						oerr := err
+						err = sess.Rollback()
+						if err != nil{
+							return fmt.Errorf("failed to rollback, original error was %s", oerr.Error())
+						}
+
+						return oerr
+					}
+				}
+
+			} else if config.Index{
+				indexFields = append(indexFields)
+			}
+		}
+
+		//create composite index
+		if len(indexFields) > 0{
+			for _, label := range structConfig.Labels{
+				numIndexCreated++
+				_, err := sess.Query().Create(dsl.NewIndex(&dsl.IndexConfig{
+					Type: label,
+					Fields: indexFields,
+				})).Exec(nil)
+				if err != nil{
+					oerr := err
+					err = sess.Rollback()
+					if err != nil{
+						return fmt.Errorf("failed to rollback, original error was %s", oerr.Error())
+					}
+
+					return oerr
+				}
+			}
+		}
+	}
+
+	log.Debugf("created (%v) indexes", numIndexCreated)
+
+	return sess.Commit()
 }
 
 //verifies all indexes
 func verifyAllIndexes() error{
+	//validate that we have to do anything
+	if mappedTypes == nil || len(mappedTypes) == 0{
+		return errors.New("must have types to map")
+	}
+
+	rows, err := dsl.QB(true).Cypher("CALL db.constraints").Query(nil)
+	if err != nil{
+		return err
+	}
+
+	rowsStr, err := dsl.RowsToStringArray(rows)
+	if err != nil{
+		return err
+	}
+
 
 }
