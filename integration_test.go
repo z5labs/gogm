ca40c9ed5d14a3233ca656964e20f710439d7ff9
Eric Solender <eric@mindstand.com>
2020-10-07 02:48:12 -0400

    change auth

--- integration_test.go
+++ integration_test.go
@@ -89,7 +89,7 @@ func TestIntegration(t *testing.T) {
 
 	conf := Config{
 		Username:      "neo4j",
-		Password:      "password",
+		Password:      "changeme",
 		Host:          "0.0.0.0",
 		IsCluster:     false,
 		Port:          7687,
